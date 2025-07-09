7fd3348538c1e86996839c34be51c95d40b49514
LucasLuizLS <ofcl.lucasluiz@gmail.com>
2025-07-09 16:10:29 -0300

    AutorDTO, EditoraDTO.

--- src/main/java/com/example/Biblioteca/dto/AutorDTO.java
+++ src/main/java/com/example/Biblioteca/dto/AutorDTO.java
@@ -6,4 +6,11 @@ import lombok.Data;
 public class AutorDTO {
     private Long id;
     private String nome;
+
+    public AutorDTO(){}
+
+    public AutorDTO(com.example.Biblioteca.model.Autor autor){
+        this.id= autor.getId();
+        this.nome= autor.getNome();
+    }
 }
--- src/main/java/com/example/Biblioteca/dto/EditoraDTO.java
+++ src/main/java/com/example/Biblioteca/dto/EditoraDTO.java
@@ -6,4 +6,11 @@ import lombok.Data;
 public class EditoraDTO {
     private Long id;
     private String nome;
+
+    public EditoraDTO(){}
+
+    public EditoraDTO(com.example.Biblioteca.model.Editora editora){
+        this.id= editora.getId();
+        this.nome= editora.getNome();
+    }
 }
