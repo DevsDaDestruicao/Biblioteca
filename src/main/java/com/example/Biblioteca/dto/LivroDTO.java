8fc375e954b35295f93b78de7a8f098bccd37617
LucasLuizLS <ofcl.lucasluiz@gmail.com>
2025-07-09 15:56:48 -0300

    LivroDTO

--- src/main/java/com/example/Biblioteca/dto/LivroDTO.java
+++ src/main/java/com/example/Biblioteca/dto/LivroDTO.java
@@ -6,8 +6,19 @@ import lombok.Data;
 public class LivroDTO {
     private Long id;
     private String titulo;
-    private String autor;
     private int ano;
+    private boolean disponivel;
+    private Long autorId;
+    private Long editoraId;
 
+    public LivroDTO(){}
+
+    public LivroDTO(com.example.Biblioteca.model.Livro livro){
+        this.id= livro.getId();
+        this.titulo= livro.getTitulo();
+        this.ano= livro.getAno();
+        this.disponivel= livro.isDisponivel();
+        this.autorId=livro.getAutor().getId();
+        this.editoraId=livro.getEditora().getId();
+    }
 }
-
--- src/main/java/com/example/Biblioteca/service/LivroService.java
+++ src/main/java/com/example/Biblioteca/service/LivroService.java
@@ -1,4 +1,5 @@
 package com.example.Biblioteca.service;
 
 public class LivroService {
+
 }
