eec7285d5c6d54e675757a83b871637363d1002e
LucasLuizLS <ofcl.lucasluiz@gmail.com>
2025-07-09 13:23:47 -0300

    teste

--- src/main/java/com/example/Biblioteca/service/LivroService.java
+++ src/main/java/com/example/Biblioteca/service/LivroService.java
@@ -1,30 +1,4 @@
 package com.example.Biblioteca.service;
 
-import com.example.Biblioteca.dto.LivroDTO;
-import com.example.Biblioteca.repository.LivroRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class LivroService {
 }
-@Autowired
-private LivroRepository livroRepository;
-
-private LivroDTO toDTO(Livro livro) {
-    LivroDTO dto = new LivroDTO();
-    dto.setId(livro.getTd());;
-    dto.setTitulo(livro.getTitulo());
-    dto.setAutor(livro.getAutor());
-    dto.setAno(livro.getAno());
-    return dto;
-}
-
-private Livro toEntity(LivroDTO dto) {
-    Livro livro= new livro();
-    livro .setId(dto.getId());
-    livro.setTitulo(dto.getTitulo());
-    livro.setAutor(dto.getAutor());
-    livro.setAno(dto.getAno());
-    return livro;
-}
-
-public list<
